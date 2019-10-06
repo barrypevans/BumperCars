dedd783b636b4e5438fcf22c19a5886b3d13e07f
uFrost <jacob.e.freeland@gmail.com>
2019-10-05 22:38:35 -0700

    Matte Color Spread Implementation
    Requires bool enable for use

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    Coroutine colorLerp;
+    int pastPaints = 0;
     public void Destroy()
     {
         Destroy(gameObject);
+    }
+    public void LerpColor(Color c)
+    {
+        if (colorLerp != null)
+            StopCoroutine(colorLerp);
+
+        colorLerp = StartCoroutine(ColorChange(c));
+    }
+    private IEnumerator ColorChange(Color c)
+    {
+        MeshRenderer m = GetComponent<MeshRenderer>();
+        Color startValue = new Color(m.material.color.r, m.material.color.g, m.material.color.b);
+        float t = 0;
+        while (t < 2f)
+        {
+            m.material.SetColor("_BaseColor",Color.Lerp(m.material.color,c,t/2f));
+            t += Time.deltaTime;
+            yield return null;
+        }
+
     }
 }
